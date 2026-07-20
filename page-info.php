c441b2980ea31853706dd2f6d78abad6ce0b7ace
Kyren82 <erykkal123@gmail.com>
2026-07-20 12:25:23 +0200

    swiper deleted

--- page-info.php
+++ page-info.php
@@ -3,7 +3,7 @@
     <div class="info-panel">
         <ul class="info-text-panel">
             <li class="info-element">
-                <p class="info-element-text"></p>
+                <p class="info-element-text">tylko nerdy na windowsie to widzą</p>
             </li>
         </ul>
     </div>
